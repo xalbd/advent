8521735c4faa00116b7cada93943a60e142d191e
Albert Dong <albertd@g.ucla.edu>
2024-12-02 22:24:56 -0800

    2024 Day 3 Refactor

--- advent24/src/day03.zig
+++ advent24/src/day03.zig
@@ -2,14 +2,7 @@ const std = @import("std");
 
 const data = @embedFile("data/day03.txt");
 
-fn isNumeric(s: []const u8) bool {
-    for (s) |c| {
-        if (c < '0' or c > '9') return false;
-    }
-    return true;
-}
-
-fn getTotal(s: []const u8) !i32 {
+fn getTotal(s: []const u8) i32 {
     var total: i32 = 0;
     var sections = std.mem.splitSequence(u8, s, "mul(");
     while (sections.next()) |section| {
@@ -18,12 +11,9 @@ fn getTotal(s: []const u8) !i32 {
 
         if (next_comma) |c| {
             if (next_rparen) |p| {
-                if (c > 0 and c < 4 and
-                    p - c > 1 and p - c < 5 and
-                    isNumeric(section[0..c]) and isNumeric(section[c + 1 .. p]))
-                {
-                    total += try std.fmt.parseInt(i32, section[0..c], 10) *
-                        try std.fmt.parseInt(i32, section[c + 1 .. p], 10);
+                if (c > 0 and c < 4 and p - c > 1 and p - c < 5) {
+                    total += (std.fmt.parseInt(i32, section[0..c], 10) catch 0) *
+                        (std.fmt.parseInt(i32, section[c + 1 .. p], 10) catch 0);
                 }
             }
         }
@@ -36,7 +26,7 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
     // part 1
-    const total1 = try getTotal(data);
+    const total1 = getTotal(data);
     try stdout.print("1: {d}\n", .{total1});
 
     // part 2
@@ -44,7 +34,7 @@ pub fn main() !void {
     var do = std.mem.splitSequence(u8, data, "do()");
     while (do.next()) |do_sec| {
         const stop = std.mem.indexOf(u8, do_sec, "don't()") orelse do_sec.len;
-        total2 += try getTotal(do_sec[0..stop]);
+        total2 += getTotal(do_sec[0..stop]);
     }
     try stdout.print("2: {d}\n", .{total2});
 }
